84509e723a39674b60d406168941152aba9d7a7e
Amelia <ameliapedersen20@gmail.com>
2024-11-15 17:56:39 -0800

    started song list

--- songs.php
+++ songs.php
@@ -1,14 +1,38 @@
-<?php 
+<?php
 $songs = [
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    []
-]
-?>
+    [
+        'cover' => '',
+        'title' => 'White Ferrari',
+        'album' => 'Blonde',
+        'artist' => 'Frank Ocean',
+        'duration' => '4:09'
+    ],
+    [
+        'cover' => '',
+        'title' => 'White Ferrari',
+        'album' => 'Blonde',
+        'artist' => 'Frank Ocean',
+        'duration' => '4:09'
+    ],
+    [
+        'cover' => '',
+        'title' => 'White Ferrari',
+        'album' => 'Blonde',
+        'artist' => 'Frank Ocean',
+        'duration' => '4:09'
+    ],
+    [
+        'cover' => '',
+        'title' => 'White Ferrari',
+        'album' => 'Blonde',
+        'artist' => 'Frank Ocean',
+        'duration' => '4:09'
+    ],
+    [
+        'cover' => '',
+        'title' => 'White Ferrari',
+        'album' => 'Blonde',
+        'artist' => 'Frank Ocean',
+        'duration' => '4:09'
+    ],
+];
