5e0013f4c91bfbbfb2533925a19d1ad8fc92a373
Amelia <ameliapedersen20@gmail.com>
2024-11-22 16:47:37 -0800

    add more song data

--- songs.php
+++ songs.php
@@ -1,35 +1,35 @@
 <?php
 $songs = [
     [
-        'cover' => '',
+        'cover' => 'images/new disturbed image.jpeg',
         'title' => 'Land of Confusion',
         'album' => 'Ten Thousand Fists',
         'artist' => 'Disturbed',
         'duration' => '3:32'
     ],
     [
-        'cover' => '',
+        'cover' => 'images/slipknot photo.jpeg',
         'title' => 'Duality',
         'album' => 'Vol.3 The Subliminal Verses',
         'artist' => 'Slipknot',
         'duration' => '4:09'
     ],
     [
-        'cover' => '',
+        'cover' => 'images/queen Latifah photo.jpeg',
         'title' => 'Latifahs Had it Up To Here',
         'album' => 'Nature of a Sista',
         'artist' => 'Queen Latifah',
         'duration' => '4:27'
     ],
     [
-        'cover' => '',
+        'cover' => 'images/greenday song.jpeg',
         'title' => 'Brain Stew',
         'album' => 'Insomniac',
         'artist' => 'GreenDay',
         'duration' => '4:09'
     ],
     [
-        'cover' => '',
+        'cover' => 'images/halestorm album.jpeg',
         'title' => 'I Am The Fire',
         'album' => 'Into the Wild Life',
         'artist' => 'Halestorm',
